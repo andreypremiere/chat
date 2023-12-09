be6e1ec9fc5de3b22b8445bd2100892dcb0c2465
Orrin19 <daniil.orrin@yandex.ru>
2023-12-09 11:19:42 +0700

    initial commit

--- src/hackathon/asgi.py
+++ src/hackathon/asgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import room.routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            room.routing.websocket_urlpatterns
+        )
+    )
+})
